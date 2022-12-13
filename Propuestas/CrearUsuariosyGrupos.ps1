793ef8157d8c1fc5fd6679f9e5945eda63c52f70
OFM <alufer7328@ieselcaminas.org>
2022-12-13 18:10:31 +0100

    Scripts y CSV

---
+++ Propuestas/CrearUsuariosyGrupos.ps1
@@ -0,0 +1,23 @@
+$file_groups=Import-Csv -Path departamentos.csv 
+foreach ($group in $file_groups) { 
+  New-LocalGroup -Name $group.nombre -Description $group.descripcion
+}
+
+$file_users=Import-Csv -Path empleado.csv 
+foreach ($user in $file_users) { 
+  $clave=ConvertTo-SecureString $user.contraseña -AsPlainText -Force
+  New-LocalUser $user.cuenta -Password $clave -FullName $user.nombre_apellidos -Description $user.descripcion -AccountNeverExpires 
+  
+  Set-LocalUser $user.cuenta -PasswordNeverExpires $false
+  net user $user.cuenta /logonpasswordchg:yes
+
+
+  #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
+
+  $Groups = ("usuarios",$user.grupo_departamento)
+
+  foreach ($Group in $Groups){
+    Add-LocalGroupMember -Group $Group -Member $user.cuenta
+  
+  }
+}
