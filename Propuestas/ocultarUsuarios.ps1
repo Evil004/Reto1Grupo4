a30ae3adc7f64ac71c57f39db0bc9cfd7b04f77d
Pere Prior <115715460+pereprior@users.noreply.github.com>
2022-12-17 17:28:22 +0100

    Update ocultarUsuarios.ps1

--- Propuestas/ocultarUsuarios.ps1
+++ Propuestas/ocultarUsuarios.ps1
@@ -3,5 +3,6 @@ $file_Users = Get-Content $fileUsersCsv
 foreach($linea in $file_Users)
 {
   REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /V $linea /T REG_DWORD /D 0
-  REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /V $env:UserName /T REG_DWORD /D 0
 }
+
+ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /V $env:UserName /T REG_DWORD /D 0
