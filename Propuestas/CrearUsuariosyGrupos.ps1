b32dafb53fc92c5cc69dce8fac4bb479e70a204c
Pere Prior <115715460+pereprior@users.noreply.github.com>
2022-12-17 10:59:21 +0100

    Update CrearUsuariosyGrupos.ps1

--- Propuestas/CrearUsuariosyGrupos.ps1
+++ Propuestas/CrearUsuariosyGrupos.ps1
@@ -1,4 +1,4 @@
-$file_groups=Import-Csv -Path departamentos.csv 
+$file_groups=Import-Csv -Path departamento.csv 
 foreach ($group in $file_groups) { 
   New-LocalGroup -Name $group.nombre -Description $group.descripcion
 }
@@ -6,14 +6,14 @@ foreach ($group in $file_groups) {
 $file_users=Import-Csv -Path empleado.csv 
 foreach ($user in $file_users) { 
   $clave=ConvertTo-SecureString $user.contraseña -AsPlainText -Force
-  New-LocalUser $user.cuenta -Password $clave -FullName $user.nombre_apellidos -PasswordNeverExpires $false -Description $user.descripcion -AccountNeverExpires 
+  New-LocalUser $user.cuenta -Password $clave -FullName $user.nombre -PasswordNeverExpires $false -AccountNeverExpires 
   
   net user $user.cuenta /logonpasswordchg:yes
 
 
   #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
 
-  $Groups = ("usuarios",$user.grupo_departamento)
+  $Groups = ("usuarios",$user.departamento)
 
   foreach ($Group in $Groups){
     Add-LocalGroupMember -Group $Group -Member $user.cuenta
